9e0403bc2dad92a3824df66839d89a13cd2add5a
Anastasia Nikitina <103180689+nasqnik@users.noreply.github.com>
2025-06-11 20:50:56 +0400

    cpp00/ex00 modification

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -3,30 +3,19 @@
 
 int main(int argc, char **argv)
 {
-    int i;
-    int j;
-
-    i = 1;
-    j = 0;
-
     if (argc == 1)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     else
     {
-        while (argv[i])
+        for (int i = 1; i < argc; i++)
         {
-            j = 0;
-            while (argv[i][j])
+            for (int j = 0; argv[i][j] != '\0'; j++)
             {
                 if (std::isalpha(argv[i][j]))
                     std::cout << (char)std::toupper(argv[i][j]);
                 else
                     std::cout << argv[i][j];
-                j++;
             }
-            if (i < argc - 1)
-                std::cout << " ";
-            i++;
         }
     }
     std::cout << std::endl;
