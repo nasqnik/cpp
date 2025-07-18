e03cf81ccb9428dc7f8eda45e2646d7819ec86b2
Anastasia Nikitina <103180689+nasqnik@users.noreply.github.com>
2025-07-18 13:15:24 +0400

    extra polishing

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -2,34 +2,45 @@
 
 #include <iostream>
 
-int main( void ) 
-{
-    std::cout << "--- Create a ---" << std::endl;
+int main( void ) {
     Fixed a;
-    std::cout << std::endl;
-
-    std::cout << "--- Create b ---" << std::endl;
-    Fixed b(a);
-    std::cout << std::endl;
-
-    std::cout << "--- Create c ---" << std::endl;
+    Fixed b( a );
     Fixed c;
     c = b;
-    std::cout << std::endl;
-    
-    std::cout << "--- Set a, b, c ---" << std::endl;
-    a.setRawBits(10);
-    b.setRawBits(20);
-    c.setRawBits(30);
-    std::cout << std::endl;
-
-    std::cout << "--- Get values of a, b, c ---" << std::endl;
     std::cout << a.getRawBits() << std::endl;
     std::cout << b.getRawBits() << std::endl;
     std::cout << c.getRawBits() << std::endl;
-    std::cout << std::endl;
-
-    std::cout << "--- Destroy a, b, c ---" << std::endl;
-
     return 0;
 }
+
+// int main( void ) 
+// {
+//     std::cout << "--- Create a ---" << std::endl;
+//     Fixed a;
+//     std::cout << std::endl;
+
+//     std::cout << "--- Create b ---" << std::endl;
+//     Fixed b(a);
+//     std::cout << std::endl;
+
+//     std::cout << "--- Create c ---" << std::endl;
+//     Fixed c;
+//     c = b;
+//     std::cout << std::endl;
+    
+//     std::cout << "--- Set a, b, c ---" << std::endl;
+//     a.setRawBits(10);
+//     b.setRawBits(20);
+//     c.setRawBits(30);
+//     std::cout << std::endl;
+
+//     std::cout << "--- Get values of a, b, c ---" << std::endl;
+//     std::cout << a.getRawBits() << std::endl;
+//     std::cout << b.getRawBits() << std::endl;
+//     std::cout << c.getRawBits() << std::endl;
+//     std::cout << std::endl;
+
+//     std::cout << "--- Destroy a, b, c ---" << std::endl;
+
+//     return 0;
+// }
